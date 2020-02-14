435c1437eeb053129add6e8d59ab755d1c65b939
davidcastel <davidccbp20@gmail.com>
2020-02-14 16:57:32 -0500

    Updates

---
+++ precache-manifest.03653344b0619172aa50f35113779b96.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35d5289bcc5f2748f68228e178f5115",
+    "url": "/davidcastel/react-portfolio-work/index.html"
+  },
+  {
+    "revision": "80b057d1a4a590b404bd",
+    "url": "/davidcastel/react-portfolio-work/static/css/main.b0a366ce.chunk.css"
+  },
+  {
+    "revision": "848eb18ac1378915f080",
+    "url": "/davidcastel/react-portfolio-work/static/js/2.483e4155.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/davidcastel/react-portfolio-work/static/js/2.483e4155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b057d1a4a590b404bd",
+    "url": "/davidcastel/react-portfolio-work/static/js/main.7c2d9697.chunk.js"
+  },
+  {
+    "revision": "458772f1f8882b022fa8",
+    "url": "/davidcastel/react-portfolio-work/static/js/runtime-main.4364284a.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/davidcastel/react-portfolio-work/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/davidcastel/react-portfolio-work/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/davidcastel/react-portfolio-work/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/davidcastel/react-portfolio-work/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "9e286e8bf57e945dbbaadff88dcd1ba5",
+    "url": "/davidcastel/react-portfolio-work/static/media/profile.9e286e8b.png"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/davidcastel/react-portfolio-work/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/davidcastel/react-portfolio-work/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/davidcastel/react-portfolio-work/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/davidcastel/react-portfolio-work/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/davidcastel/react-portfolio-work/static/media/twitter_icon.512536cb.png"
+  }
+]);
--- src/projects/music-master/index.js
+++
@@ -1,5 +0,0 @@
-// import React from "react"; // reaches into react library in the node-module
-import App from "./components/app";
-import "./index.css";
-
-export default App;
